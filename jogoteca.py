94ebed03693f79e9ab2044b672989884a7305bd9
Gabi Desktop <gabii12303.net@gmail.com>
2023-07-14 21:32:53 -0300

    .

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    return '<h1>Olá Mundo!</h1>'
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 app.run()
